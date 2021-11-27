003d10a18451ca38183ee1c209831e3dda3d1fd9
chris <chris@bumblehead.com>
2021-11-26 16:15:15 -0800

    change interface

--- src/esmock.js
+++ src/esmock.js
@@ -11,28 +11,26 @@ import {
 const argHasKey = (arg, key) => (
   arg && !/number|boolean/.test(typeof arg) && key in arg);
 
-const esmock = async (modulePath, mockDefs, globalDefs, opt) => {
-  // this functions caller is stack item '2'
-  const calleePath = new Error().stack.split('\n')[2]
+const esmock = async (modulePath, mockDefs, globalDefs, opt = {}, err) => {
+  // callee path is stack item '2'
+  const calleePath = (err || new Error()).stack.split('\n')[2]
     .replace(/^.*file:\/\//, '') // rm everything before filepathfe
     .replace(/:[\d]*:[\d]*.*$/, '') // rm line and row number
     .replace(/^.*:/, ''); // rm windows-style drive locations
 
-  // remap params in case options are provided as first arg
-  [ opt, mockDefs, globalDefs ] = argHasKey(mockDefs, 'isPurge')
-    ? [ mockDefs || {}, globalDefs || {}, opt || {} ]
-    : [ opt || {}, mockDefs || {}, globalDefs || {} ];
-
   const modulePathKey = await esmockModuleMock(
-    calleePath, modulePath, mockDefs || {}, globalDefs || {}, opt || {});
+    calleePath, modulePath, mockDefs || {}, globalDefs || {}, opt);
 
   const importedModule = await import(modulePathKey);
 
-  if (opt.isPurge !== false)
+  if (opt.purge !== false)
     esmockModuleImportedPurge(modulePathKey);
-  
+
   return esmockModuleImportedSanitize(importedModule, modulePathKey);
 };
+
+esmock.p = async (modulePath, mockDefs, globalDefs) => (
+  esmock(modulePath, mockDefs, globalDefs, { purge : false }, new Error()));
 
 esmock.purge = mockModule => {
   if (argHasKey(mockModule, 'esmockKey'))
