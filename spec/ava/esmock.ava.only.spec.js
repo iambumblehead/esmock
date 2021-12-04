17ab7b2dfe63c6bdf498d0b59ed5e5519ef4a1db
chris <chris@bumblehead.com>
2021-12-04 00:55:17 -0800

    added uvu tests

--- spec/ava/esmock.ava.only.spec.js
+++ spec/ava/esmock.ava.only.spec.js
@@ -1,10 +1,10 @@
 import test from 'ava';
-import esmock from '../src/esmock.js';
+import esmock from '../../src/esmock.js';
 
 // this error can occur when sources do not define 'esmockloader'
 // on 'global' but use a process linked variable instead
 test.only('should not error when esmock used with ava.only', async t => {
-  await esmock('./local/mainUtil.js', {
+  await esmock('../local/mainUtil.js', {
     'form-urlencoded' : () => 'mock encode'
   });
 
