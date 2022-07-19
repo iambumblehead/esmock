8c05a722757cb7b61cb75d9f16788c2d8fb81c83
chris <chris@bumblehead.com>
2022-07-19 13:45:55 -0700

    log specifier and context to see what is printed to ci

--- src/esmockLoader.mjs
+++ src/esmockLoader.mjs
@@ -33,6 +33,7 @@ const resolve = async (specifier, context, nextResolve) => {
   const [ esmockKeyParam ] =
     (esmockKeyLong && esmockKeyLong.match(esmockKeyRe) || []);
 
+  console.log(specifier, context);
   // new versions of node: when multiple loaders are used and context
   // is passed to nextResolve, the process crashes in a recursive call
   // see: /esmock/issues/#48
