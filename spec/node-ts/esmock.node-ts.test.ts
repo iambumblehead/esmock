c696f55c37d6596c50bc6f85bd3d1b0986cc82be
iambumblehead <cdepauw@gmail.com>
2022-07-19 16:02:30 -0700

    remove copy/pasted comment

--- spec/node-ts/esmock.node-ts.test.ts
+++ spec/node-ts/esmock.node-ts.test.ts
@@ -2,8 +2,6 @@ import test from 'node:test';
 import assert from 'assert';
 import esmock from '../../src/esmock.js';
 
-// this error can occur when sources do not define 'esmockloader'
-// on 'global' but use a process linked variable instead
 test('should mock ts when using node-ts', { only : true }, async () => {
   const main = await esmock('../local/main.ts', {
     path: {
