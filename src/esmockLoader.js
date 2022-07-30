e03fd55774ad50ec80aa555ca159452ebe44f8f3
chris <chris@bumblehead.com>
2022-07-30 10:46:52 -0700

    update condition

--- src/esmockLoader.js
+++ src/esmockLoader.js
@@ -46,23 +46,18 @@ const resolve = async (specifier, context, nextResolve) => {
   // is not passed to nextResolve, the tests fail
   //
   // later versions of node v16 include 'node-addons'
-  //  const resolved = context.parentURL &&
-  //        ( isLT1612 ||
-  //          (context.importAssertions && context.conditions.slice(-1)[0] === 'node-addons') )
-  //    ? await nextResolve(specifier, context)
-  //    : await nextResolve(specifier);
-  // const resolved = (context.conditions.slice(-1)[0] === 'node-addons' &&
-  //   ((context.importAssertions && context.parentURL) || isLT1612)) || context.parentURL
+  const resolved = context.parentURL && (
+    context.conditions.slice(-1)[0] !== 'node-addons'
+      || ((context.importAssertions && context.parentURL) || isLT1612))
+    ? await nextResolve(specifier, context)
+    : await nextResolve(specifier);
+  // const resolved = context.conditions.slice(-1)[0] === 'node-addons'
+  //   ? (((context.importAssertions && context.parentURL) || isLT1612)
   //     ? await nextResolve(specifier, context)
-  //     : await nextResolve(specifier);
-
-  const resolved = context.conditions.slice(-1)[0] === 'node-addons'
-    ? (((context.importAssertions && context.parentURL) || isLT1612)
-      ? await nextResolve(specifier, context)
-      : await nextResolve(specifier))
-    : (context.parentURL
-      ? await nextResolve(specifier, context)
-      : await nextResolve(specifier));
+  //     : await nextResolve(specifier))
+  //   : (context.parentURL
+  //     ? await nextResolve(specifier, context)
+  //     : await nextResolve(specifier));
 
   if (!esmockKeyParam)
     return resolved;
