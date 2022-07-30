af1e8b41f1d32b0e28367ddf812abc879609b5b6
chris <chris@bumblehead.com>
2022-07-30 10:38:00 -0700

    update condition

--- src/esmockLoader.js
+++ src/esmockLoader.js
@@ -46,11 +46,16 @@ const resolve = async (specifier, context, nextResolve) => {
   // is not passed to nextResolve, the tests fail
   //
   // later versions of node v16 include 'node-addons'
-  const resolved = context.parentURL &&
-        ( isLT1612 ||
-          (context.importAssertions && context.conditions.slice(-1)[0] === 'node-addons') )
-    ? await nextResolve(specifier, context)
-    : await nextResolve(specifier);
+  //  const resolved = context.parentURL &&
+  //        ( isLT1612 ||
+  //          (context.importAssertions && context.conditions.slice(-1)[0] === 'node-addons') )
+  //    ? await nextResolve(specifier, context)
+  //    : await nextResolve(specifier);
+  const resolved = context.conditions.slice(-1)[0] === 'node-addons' &&
+    (((context.importAssertions && context.parentURL) || isLT1612) || context.parentURL)
+      ? await nextResolve(specifier, context)
+      : await nextResolve(specifier);
+
   // const resolved = context.conditions.slice(-1)[0] === 'node-addons'
   //   ? (((context.importAssertions && context.parentURL) || isLT1612)
   //     ? await nextResolve(specifier, context)
