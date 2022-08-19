66a9474cda586d5ec1fa2ca4e08336f1491eaf9c
chris <chris@bumblehead.com>
2022-08-19 07:35:53 -0700

    reproduce not a valid path error

--- tests/local/main.ts
+++ tests/local/main.ts
@@ -1,5 +1,7 @@
 import path from 'path';
+import pg from 'pg';
 
 export default {
-  pathbasenamewrap: (n: any) => path.basename(n)
+  pathbasenamewrap: (n: any) => path.basename(n),
+  pgpoolwrap: (n: any) => pg.Pool(n)
 };
--- tests/tests-tsm/esmock.node-tsm.test.ts
+++ tests/tests-tsm/esmock.node-tsm.test.ts
@@ -2,7 +2,7 @@ import test from 'node:test';
 import assert from 'assert';
 import esmock from 'esmock';
 
-test('should mock ts when using node-ts', { only : true }, async () => {
+test('should mock ts when using node-ts', async () => {
   const main = await esmock('../local/main.ts', {
     path: {
       basename: () => 'hellow'
@@ -11,4 +11,16 @@ test('should mock ts when using node-ts', { only : true }, async () => {
 
   assert.strictEqual( main.pathbasenamewrap(), 'hellow' );
   assert.ok(true);
+});
+
+test('should mock pg', async () => {
+  const main = await esmock('../local/main-pg.ts', {
+    pg: {
+      Pool: (config:any) => {
+         return 'mocked pool'
+      }
+    }
+  });
+
+  assert.strictEqual( main.pgpoolwrap(), 'mocked pool' );
 });
