f1e6f330f6e46b3c107ff7fcff423c3e46664c4f
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-09-26 13:10:23 -0500

    refactor: deduplicate JSDoc
    Ref: https://github.com/iambumblehead/esmock/pull/163#pullrequestreview-1120344255

--- src/esmock.d.ts
+++ src/esmock.d.ts
@@ -1,71 +1,85 @@
-type mocks = Record<string, any>
+type MockMap = { [specifier: string]: any };
 
-type opts = {
-  strict?: boolean | undefined,
-  purge?: boolean | undefined,
-  isModuleNotFoundError?: boolean | undefined
-}
+type Options = {
+  strict?: boolean | undefined;
+  purge?: boolean | undefined;
+  isModuleNotFoundError?: boolean | undefined;
+};
 
 /**
  * Mocks imports for the module specified by {@link modulePath}.
  *
+ * @param modulePath The module whose imports will be mocked.
+ *
+ * @param parent A URL used to resolve relative specifiers, typically
+ * `import.meta.url`. If not specified, inferred via the stack. Useful with
+ * source maps.
+ *
+ * @param defs A mapping of import specifiers to mock definitions.
+ *
+ * @param gdefs A globally applied mapping of import specifiers to mock
+ * definitions.
+ *
+ * @param opts
+ *
+ * @returns The result of importing {@link modulePath}, similar to
+ * `import(modulePath)`.
+ */
+type MockFunction = {
+  (
+    modulePath: string,
+    parent: string,
+    defs?: MockMap,
+    gdefs?: MockMap,
+    opts?: Options,
+  ): any;
+  (
+    modulePath: string,
+    defs?: MockMap,
+    gdefs?: MockMap,
+    opts?: Options,
+  ): any;
+};
+
+/**
  * By default, mock definitions are merged with the original module definitions.
  * To avoid the default behaviour, use esmock.strict.
- *
- * @param modulePath The module whose imports will be mocked.
- * @param parent A URL to resolve specifiers relative to; typically `import.meta.url`.
- *               If not specified, inferred via the stack. Useful with source maps.
- * @param defs A mapping of import specifiers to mock definitions.
- * @param gdefs A mapping of import specifiers to mock definitions, applied globally.
- * @param opt
- * @returns The result of importing {@link modulePath}, similar to `import(modulePath)`.
  */
-declare function esmock(modulePath: string, parent: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-declare function esmock(modulePath: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-
-declare namespace esmock {
-  /**
-   * Mocks imports for the module specified by {@link modulePath}.
-   *
-   * The "strict" variant replaces original module definitions with mock definitions.
-   *
-   * @param modulePath The module whose imports will be mocked.
-   * @param parent A URL to resolve specifiers relative to; typically `import.meta.url`.
-   *               If not specified, inferred via the stack. Useful with source maps.
-   * @param defs A mapping of import specifiers to mock definitions.
-   * @param gdefs A mapping of import specifiers to mock definitions, applied globally.
-   * @param opt
-   * @returns The result of importing {@link modulePath}, similar to `import(modulePath)`.
-   */
-  function strict(modulePath: string, parent: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-  function strict(modulePath: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-  export namespace strict {
-    function p(modulePath: string, parent: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-    function p(modulePath: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-  }
-
+declare const esmock: MockFunction & {
   /**
    * Uses caching to support `await import()` inside the mocked import tree.
    *
-   * After using this function, consider calling {@link esmock.purge} to free memory.
-   *
-   * @param modulePath The module whose imports will be mocked.
-   * @param parent A URL to resolve specifiers relative to; typically `import.meta.url`.
-   *               If not specified, inferred via the stack. Useful with source maps.
-   * @param defs A mapping of import specifiers to mock definitions.
-   * @param gdefs A mapping of import specifiers to mock definitions, applied globally.
-   * @param opt
-   * @returns The result of importing {@link modulePath}, similar to `import(modulePath)`.
+   * After using this function, consider calling {@link esmock.purge}
+   * to free memory.
    */
-  function p(modulePath: string, parent: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
-  function p(modulePath: string, defs?: mocks, gdefs?: mocks, opt?: opts): any
+  p: MockFunction;
 
   /**
    * Removes caching created by {@link esmock.p}.
    *
    * @param mockModule A module object returned from {@link esmock.p}.
    */
-  function purge(mockModule: any): void
-}
+  purge: (mockModule: any) => void;
 
-export { esmock as default, esmock as strict }
+  /**
+   * The "strict" variant replaces original module definitions
+   * with mock definitions.
+   */
+  strict: MockFunction & {
+    p: MockFunction;
+  };
+};
+
+/**
+ * The "strict" variant replaces original module definitions
+ * with mock definitions.
+ */
+declare const strict: typeof esmock['strict'];
+
+export {
+  esmock as default,
+  strict,
+  type MockFunction,
+  type MockMap,
+  type Options,
+};
