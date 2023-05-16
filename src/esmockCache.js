0a2a93a2093421534714487d65131f56f722e859
chris <chris@bumblehead.com>
2023-05-15 23:36:36 -0700

    adding worker thread messaging (try)

--- src/esmockCache.js
+++ src/esmockCache.js
@@ -7,6 +7,8 @@ const esmockCache = {
 }
 
 const esmockTreeIdSet = (key, keylong) => (
+  (typeof global.postMessageEsmk === 'function')
+    && global.postMessageEsmk({ key, keylong }),
   global.mockKeys[String(key)] = keylong)
 
 const esmockTreeIdGet = key => (
--- src/esmockLoader.js
+++ src/esmockLoader.js
@@ -1,4 +1,5 @@
 import process from 'process'
+import { isMainThread } from 'node:worker_threads'
 import urlDummy from './esmockDummy.js'
 import esmockErr from './esmockErr.js'
 
@@ -15,6 +16,13 @@ const exportNamesRe = /.*exportNames=(.*)/
 const withHashRe = /.*#-#/
 const isesmRe = /isesm=true/
 const isnotfoundRe = /isfound=false/
+
+const globalPreload = isMainThread && (({ port }) => (
+  port.addEventListener('message', ev => (
+    global.mockKeys[ev.data.key] = ev.data.keylong)),
+  port.unref(),
+  'global.postMessageEsmk = d => port.postMessage(d)'
+))
 
 // new versions of node: when multiple loaders are used and context
 // is passed to nextResolve, the process crashes in a recursive call
@@ -123,4 +131,4 @@ const load = async (url, context, nextLoad) => {
 // node lt 16.12 require getSource, node gte 16.12 warn remove getSource
 const getSource = isLT1612 && load
 
-export {load, resolve, getSource, loaderIsVerified}
+export {load, resolve, getSource, loaderIsVerified, globalPreload}
