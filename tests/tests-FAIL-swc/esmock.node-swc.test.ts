17b6cab851d619b8b81cfaf5af73263880c23d5e
chris <chris@bumblehead.com>
2023-08-14 19:35:58 -0700

    cleanup node-swc.test.ts file

--- tests/tests-FAIL-swc/esmock.node-swc.test.ts
+++ tests/tests-FAIL-swc/esmock.node-swc.test.ts
@@ -2,22 +2,20 @@ import test from 'node:test'
 import assert from 'assert'
 import esmock from 'esmock'
 
+// ⚠ https://github.com/swc-project/swc-node/issues/710
+// swc tests fail when encountering typescript syntax
+// when the typescript sytax are removed, tests pass
+
 test('should mock ts when using node-ts', async () => {
-    const main = await import('../local/main.ts')
-    // const main = await import('../local/mainUtil.js')
-    console.log(main)
-  // const main = await import('../local/main.js')
-  /*
   const main = await esmock('../local/main.ts', {
     path: {
       basename: () => 'hellow'
     }
-  })*/
+  })
 
-  // assert.strictEqual(main.pathbasenamewrap(), 'hellow')
-  assert.ok(true)
+  assert.strictEqual(main.pathbasenamewrap(), 'hellow')
 })
-/*
+
 test('should mock pg', async () => {
   const main = await esmock('../local/main.ts', {
     'pg': {
@@ -29,4 +27,3 @@ test('should mock pg', async () => {
 
   assert.strictEqual(main.pgpoolwrap(), 'mocked pool')
 })
-*/
