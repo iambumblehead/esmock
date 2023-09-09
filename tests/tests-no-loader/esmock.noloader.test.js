264b9818a4e0ab81a5d57d5a908c99a78ab5aab2
chris <chris@bumblehead.com>
2023-09-09 13:01:39 -0700

    use node version to define noloader test

--- tests/tests-no-loader/esmock.noloader.test.js
+++ tests/tests-no-loader/esmock.noloader.test.js
@@ -1,13 +1,14 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
-import module from 'node:module'
-import threads from 'node:worker_threads'
 import esmock from 'esmock'
 import esmockErr from '../../src/esmockErr.js'
 
-// newer versions of node support module.register, do not need --loader
-// older versions of node need --loader
-if (module.register && threads.MessageChannel) {
+// node version 20.6+ do not need --loader
+const isLT206 = (([major, minor]) => (
+  major < 20 || (major === 20 && minor < 6)))(
+  process.versions.node.split('.').map(it => +it))
+
+if (isLT206) {
   test('should mock a module', async () => {
     const main = await esmock('../local/mainUtil.js', {
       'form-urlencoded': () => 'mock encode'
