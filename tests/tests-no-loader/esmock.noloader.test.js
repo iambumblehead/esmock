4d42f38b0c1ad5f05b197fe720c86c39d7b21af6
chris <chris@bumblehead.com>
2023-09-09 00:50:14 -0700

    added condition for noloader test, expect error only when register not supported

--- tests/tests-no-loader/esmock.noloader.test.js
+++ tests/tests-no-loader/esmock.noloader.test.js
@@ -1,10 +1,24 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
+import module from 'node:module'
 import esmock from 'esmock'
 import esmockErr from '../../src/esmockErr.js'
 
-test('should throw error if !esmockloader', async () => {
-  await assert.rejects(() => esmock('./to/module.js'), {
-    message: esmockErr.errMissingLoader().message
+// newer versions of node support module.register, do not need --loader
+// older versions of node need --loader
+if (module.register) {
+  test('should mock a module', async () => {
+    const main = await esmock('../local/mainUtil.js', {
+      'form-urlencoded': () => 'mock encode'
+    })
+
+    assert.strictEqual(typeof main, 'function')
+    assert.strictEqual(main.createString(), 'mock encode')
   })
-})
+} else {
+  test('should throw error if !esmockloader', async () => {
+    await assert.rejects(() => esmock('./to/module.js'), {
+      message: esmockErr.errMissingLoader().message
+    })
+  })
+}
