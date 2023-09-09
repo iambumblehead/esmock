51f08caddfc31b18fa5e7e3a3db4d0cf6c935892
chris <chris@bumblehead.com>
2023-09-09 12:51:01 -0700

    update feature detection for no loader test

--- tests/tests-no-loader/esmock.noloader.test.js
+++ tests/tests-no-loader/esmock.noloader.test.js
@@ -1,12 +1,13 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 import module from 'node:module'
+import threads from 'node:worker_threads'
 import esmock from 'esmock'
 import esmockErr from '../../src/esmockErr.js'
 
 // newer versions of node support module.register, do not need --loader
 // older versions of node need --loader
-if (module.register) {
+if (module.register && threads.MessageChannel) {
   test('should mock a module', async () => {
     const main = await esmock('../local/mainUtil.js', {
       'form-urlencoded': () => 'mock encode'
