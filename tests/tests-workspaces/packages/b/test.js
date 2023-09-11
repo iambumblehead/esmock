c6ad1457ddf2dcc78ae1e3fcac9545849037f573
chris <chris@bumblehead.com>
2023-09-11 01:24:13 -0700

    use local esmock

--- tests/tests-workspaces/packages/b/test.js
+++ tests/tests-workspaces/packages/b/test.js
@@ -1,7 +1,7 @@
-import esmock from 'esmock'
 import module from 'module'
 import test from 'node:test'
 import assert from 'node:assert/strict'
+import esmock from '../../../../src/esmock.js'
 
 test('works ootb', async () => {
   const sut = await esmock('./index.js', {
