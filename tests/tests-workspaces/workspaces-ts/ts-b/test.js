9d28c026e4885cb1fb196594d596642523fcf3bc
chris <chris@bumblehead.com>
2023-09-12 18:39:39 -0700

    use separate js and ts workspaces

---
+++ tests/tests-workspaces/workspaces-ts/ts-b/test.js
@@ -0,0 +1,11 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import esmock from '../../../../src/esmock.js'
+
+test('works ootb', async () => {
+  const sut = await esmock('./index.js', {
+    'js-a': { foo: () => 'bar' }
+  })
+
+  assert.equal(sut(), 'bar')
+})
