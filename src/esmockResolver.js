a4c226e337eea787436e2655c263b4b0eea1e289
Vladimir Grenaderov <kosh@vorlon.co>
2023-10-14 19:42:43 +0000

    fix formatting

--- src/esmockResolver.js
+++ src/esmockResolver.js
@@ -1,5 +1,6 @@
 import resolvewithplus from 'resolvewithplus'
 
-const pnpResolver = process.versions.pnp && (await import('./pnpResolver.js')).default;
+const pnpResolver = process.versions.pnp &&
+  (await import('./pnpResolver.js')).default
 
 export default async opts => opts.resolver || pnpResolver || resolvewithplus
