4c58d6c14f285415f2685ea3d54776df71722acb
chris <chris@bumblehead.com>
2024-07-16 14:08:43 -0700

    update test to use esmock, to pass lint, to give credit

--- tests/tests-swc/esmock.node-swc.test.ts
+++ tests/tests-swc/esmock.node-swc.test.ts
@@ -2,10 +2,7 @@ import test from 'node:test'
 import assert from 'assert'
 import esmock from 'esmock'
 
-// ⚠ https://github.com/swc-project/swc-node/issues/710
-// swc tests fail when encountering typescript syntax
-// when the typescript sytax are removed, tests pass
-
+// credit @Brooooooklyn https://github.com/swc-project/swc-node/issues/788
 test('should mock js when using swc', async () => {
   const main = await esmock('../local/main.js', {
     path: {
@@ -17,17 +14,11 @@ test('should mock js when using swc', async () => {
 })
 
 test('should mock ts when using swc', async () => {
-  const main = await import('../local/main-ts.ts')
-/*
   const main = await esmock('../local/main-ts.ts', {
     path: {
       basename: () => 'hellow'
     }
   })
-*/
-  assert.strictEqual(
-    main.default.pathbasenamewrap('/path/to/hellow'),
-    'hellow')
-//  assert.strictEqual(main.pathbasenamewrap(), 'hellow')
 
+  assert.strictEqual(main.pathbasenamewrap(), 'hellow')
 })
