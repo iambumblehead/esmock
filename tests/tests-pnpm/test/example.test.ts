54574f183cc07ea5629bde62f9393dbb57692fce
chris <chris@bumblehead.com>
2024-10-17 18:26:42 -0700

    rename test

--- tests/tests-pnpm/test/example.test.ts
+++ tests/tests-pnpm/test/example.test.ts
@@ -3,7 +3,7 @@ import { equal } from "node:assert/strict"
 import esmock from "esmock"
 
 describe("Example", async () => {
-  it("Fails", async () => {
+  it("Mocks scoped module", async () => {
     const { example } = await esmock(
       "../src/example.js",
       import.meta.url,
